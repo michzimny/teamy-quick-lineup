542e7cfbf6c543a234309feb42d087779a7cdc12
emkael <emkael@tlen.pl>
2016-11-25 22:09:22 +0100

    Configuration from external JSON file. Fixes #2

--- ql/settings.py
+++ ql/settings.py
@@ -1,12 +1,33 @@
+import json, sys
+
+def _fetch_settings(config_path, constant_config, default_config):
+    try:
+        config = constant_config.copy()
+        config.update(json.load(open(config_path)))
+        return config
+    except FileNotFoundError:
+        with open(config_path, 'w') as new_config:
+            json.dump(default_config, new_config)
+        print('Config file created, fill it up!')
+        sys.exit()
+    except ValueError:
+        print('Config file invalid, fix it!')
+        sys.exit(1)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'your-username',
-        'PASSWORD': 'your-password',
-        'NAME': 'your-database-name',
-    }
+    'default': _fetch_settings(
+        'config.json',
+        {
+            'ENGINE': 'mysql.connector.django'
+        },
+        {
+            'HOST': 'localhost',
+            'PORT': '3306',
+            'USER': 'root',
+            'PASSWORD': '',
+            'NAME': 'belongs_to_us'
+        }
+    )
 }
 
 INSTALLED_APPS = (
