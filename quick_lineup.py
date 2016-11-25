33159ab536d15adab09da18e0012889023887ec9
emkael <emkael@tlen.pl>
2016-11-25 22:27:05 +0100

    Wrapping app entry point in method

--- quick_lineup.py
+++ quick_lineup.py
@@ -2,18 +2,23 @@ import sys
 from ql.console import Console
 
 
-if len(sys.argv) < 3 or len(sys.argv) > 4:
-    print('Give correct parameters: round, segment and (optionally) table')
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print('Give correct parameters: round, segment and (optionally) table')
+        sys.exit(1)
 
-round = int(sys.argv[1])
-segment = int(sys.argv[2])
-if len(sys.argv) == 4:
-    table = int(sys.argv[3])
-else:
-    table = None
+    round = int(sys.argv[1])
+    segment = int(sys.argv[2])
+    if len(sys.argv) == 4:
+        table = int(sys.argv[3])
+    else:
+        table = None
 
-try:
-    Console(round, segment, table).run()
-except:
-    print('ERROR: %s' % sys.exc_info()[1])
+    try:
+        Console(round, segment, table).run()
+    except:
+        print('ERROR: %s' % sys.exc_info()[1])
+
+
+if __name__ == '__main__':
+    main()
